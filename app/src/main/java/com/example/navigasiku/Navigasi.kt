f9eb764cb9f970f0f212b5fc7b673e9533fdbe37
Lenovo <9.3razzinayyuri@gmail.com>
2025-08-12 17:15:51 +0700

    first commit

---
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -0,0 +1,50 @@
+package com.example.navigasiku
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasiku.view.FormIsi
+import com.example.navigasiku.view.TampilData
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@Composable
+fun DataApp(navController: NavHostController = rememberNavController()
+){
+    Scaffold() { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
+            composable(route = Navigasi.Formulir.name) {
+                FormIsi(
+                    //PilihanJK = jenisK.map {id -> konteks.resources.getString(id)},
+                    onSubmitbtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+                TampilData(
+                    onBackBtnClick = { cancelAndBackToFormulir(navController) }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulir(navController: NavHostController) {
+    navController.popBackStack(route = Navigasi.Formulir.name, inclusive = false)
+}
+
+
